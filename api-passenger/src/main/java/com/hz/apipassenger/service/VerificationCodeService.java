589eadc5fd572ee97c88a246e8ebaff8a2072316
huangzhong888 <2807962846@qq.com>
2022-10-30 21:59:54 +0800

    校验验证码

--- api-passenger/src/main/java/com/hz/apipassenger/service/VerificationCodeService.java
+++ api-passenger/src/main/java/com/hz/apipassenger/service/VerificationCodeService.java
@@ -1,9 +1,11 @@
 package com.hz.apipassenger.service;
 
 import com.hz.apipassenger.remote.ServiceVerificationCodeClient;
+import com.hz.internal.common.constant.CommonStatusEnum;
 import com.hz.internal.common.dto.ResponseResult;
 import com.hz.internal.common.response.NumberCodeResponse;
 import com.hz.internal.common.response.TokenResponse;
+import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
@@ -38,7 +40,7 @@ public class VerificationCodeService {
         int numberCode = numberCodeResponse.getData().getNumberCode();
 
         //key,value,过期时间
-        String key = verificationCodePrefix + passengerPhone;
+        String key = generateKeyByPhone(passengerPhone);
         // 存入Redis
         stringRedisTemplate.opsForValue().set(key,numberCode+"",2, TimeUnit.MINUTES);
 
@@ -49,6 +51,13 @@ public class VerificationCodeService {
     }
 
     /**
+     * 根据手机号生成key
+     * @return
+     */
+    public String generateKeyByPhone(String passengerPhone){
+        return verificationCodePrefix + passengerPhone;
+    }
+    /**
      * 校验验证码
      * @param passengerPhone 手机号
      * @param verificationCode 验证码
@@ -57,7 +66,19 @@ public class VerificationCodeService {
     public ResponseResult checkCode(String passengerPhone,String verificationCode){
         //根据手机号去redis读取验证码
         System.out.println("根据手机号去redis读取验证码");
+        //生成key
+        String key = generateKeyByPhone(passengerPhone);
+        //根据key获取value
+        String codeRedis = stringRedisTemplate.opsForValue().get(key);
+        System.out.println("redis中的value"+codeRedis);
         //对传进来的验证码进行校验
+        if(StringUtils.isBlank(codeRedis)){
+            return ResponseResult.fail(CommonStatusEnum.FAIL.getCode(),CommonStatusEnum.FAIL.getValue());
+        }
+        if(!verificationCode.trim().equals(codeRedis)){
+            return ResponseResult.fail(CommonStatusEnum.FAIL.getCode(),CommonStatusEnum.FAIL.getValue());
+
+        }
         System.out.println("对传进来的验证码进行校验");
         //判断原来是否有用户，有即为登录，否则注册
         System.out.println("判断原来是否有用户，有即为登录，否则注册");
--- internal-common/src/main/java/com/hz/internal/common/constant/CommonStatusEnum.java
+++ internal-common/src/main/java/com/hz/internal/common/constant/CommonStatusEnum.java
@@ -9,6 +9,8 @@ import lombok.Getter;
  * @version: 1.0
  */
 public enum CommonStatusEnum {
+    //验证码错误提示
+    VERIFICATION_CODE_ERROR(1099,"验证码不正确"),
     SUCCESS(1,"success"),
     FAIL(0,"fail")
     ;
