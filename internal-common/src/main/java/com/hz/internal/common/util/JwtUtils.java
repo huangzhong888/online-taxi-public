25b63e3e6e88494f4e8341265e4f7434aba56c6b
huangzhong888 <2807962846@qq.com>
2022-11-01 18:25:10 +0800

    Jwt工具，token的解析

--- internal-common/src/main/java/com/hz/internal/common/util/JwtUtils.java
+++ internal-common/src/main/java/com/hz/internal/common/util/JwtUtils.java
@@ -3,9 +3,12 @@ package com.hz.internal.common.util;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.JWTCreator;
 import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.interfaces.Claim;
+import com.auth0.jwt.interfaces.DecodedJWT;
 
 import java.util.Calendar;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -15,12 +18,19 @@ import java.util.Map;
  * @version: 1.0
  */
 public class JwtUtils {
+    public static final String JWT_KEY = "passengerPhone";
 
     //盐
     public static final String SIGN = "hz!@#$";
 
-    //生成token
-    private static String generatorToken(Map<String,String> map){
+    /**
+     * 生成toke
+     * @param passengerPhone
+     * @return
+     */
+    private static String generatorToken(String passengerPhone){
+        Map<String,String> map = new HashMap<>();
+        map.put("JWT_KEY",passengerPhone);
         //token过期时间
         Calendar calendar = Calendar.getInstance();
         calendar.add(Calendar.DATE,1);
@@ -31,8 +41,8 @@ public class JwtUtils {
         //整合map
         map.forEach(
                 (k,v)->{
-            builder.withClaim(k,v);
-            }
+                    builder.withClaim(k,v);
+                }
         );
         //整合过期时间
         builder.withExpiresAt(date);
@@ -40,5 +50,16 @@ public class JwtUtils {
         //生成token
         String sign = builder.sign(Algorithm.HMAC256(SIGN));
         return sign;
+    }
+
+    /**
+     * 解析token
+     * @param token
+     * @return
+     */
+    public static String parseToken(String token){
+        DecodedJWT verify = JWT.require(Algorithm.HMAC256(SIGN)).build().verify(token);
+        Claim claim = verify.getClaim(JWT_KEY);
+        return claim.toString();
     }
 }
