913dc3a405e5d4a03f695c3343bed4b869f01085
huangzhong888 <2807962846@qq.com>
2022-11-01 10:27:01 +0800

    插入用户

--- service-passenger-user/src/main/java/com/hz/servicepassengeruser/service/UserService.java
+++ service-passenger-user/src/main/java/com/hz/servicepassengeruser/service/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -25,13 +26,26 @@ public class UserService {
     private PassengerUserMapper passengerUserMapper;
 
     public ResponseResult loginOrRegister(String passengerPhone ){
-        //进行匹配校验,查询用户
+        //进行匹配校验,查询用户信息
         Map<String , Object> map = new HashMap<>();
         map.put("passenger_phone",passengerPhone);
         List<PassengerUser> passengerUsers = passengerUserMapper.selectByMap(map);
-        System.out.println(passengerUsers.size()==0?"无记录":passengerUsers.get(0).getPassengerPhone());
-        System.out.println("user service被调用");
-        //如果不存在，新增用户
+       
+        //判断用户是否存在
+        if(passengerUsers.size()==0){
+            //用户不存在，新增用户
+            PassengerUser passengerUser = new PassengerUser();
+            passengerUser.setPassengerName("张三");
+            LocalDateTime now = LocalDateTime.now();
+            passengerUser.setGmtCreate(now);
+            passengerUser.setGmtModified(now);
+            passengerUser.setPassengerGender((byte) 0);
+            passengerUser.setPassengerPhone(passengerPhone);
+            passengerUser.setState((byte)0);
+
+            passengerUserMapper.insert(passengerUser);
+        }
+
 
         //存在，则登录
         return ResponseResult.success();
