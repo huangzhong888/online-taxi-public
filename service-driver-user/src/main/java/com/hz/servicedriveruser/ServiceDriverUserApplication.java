554401132ffd563823531691405c189d19f19769
huangzhong888 <2807962846@qq.com>
2022-12-12 20:01:22 +0800

    service-driver-user注册到nacos

--- service-driver-user/src/main/java/com/hz/servicedriveruser/ServiceDriverUserApplication.java
+++ service-driver-user/src/main/java/com/hz/servicedriveruser/ServiceDriverUserApplication.java
@@ -3,6 +3,7 @@ package com.hz.servicedriveruser;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @Auther: huangzhong
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @version: 1.0
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 @MapperScan("com.hz.servicedriveruser.mapper")
 public class ServiceDriverUserApplication {
     public static void main(String[] args) {
