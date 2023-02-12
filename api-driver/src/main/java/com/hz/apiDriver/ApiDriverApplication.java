1151c2115de60f43d06d65e880a454d447d5cfee
huangzhong888 <2807962846@qq.com>
2023-02-12 10:19:46 +0800

    api-driver服务创建

---
+++ api-driver/src/main/java/com/hz/apiDriver/ApiDriverApplication.java
@@ -0,0 +1,24 @@
+package com.hz.apiDriver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Auther: huangzhong
+ * @Date: 2023/2/12 - 02 - 12 - 10:14
+ * @Description: com.hz.apiDriver
+ * @version: 1.0
+ */
+@SpringBootApplication
+@RestController
+public class ApiDriverApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiDriverApplication.class);
+    }
+    @GetMapping("test")
+    public String test(){
+        return "test";
+    }
+}
